6696e95f792b8958ac7ab02f466715e0fbb50ec1
Nocho5 <shanewaters125@gmail.com>
2022-12-21 16:54:33 +0000

    test coverage

---
+++ src/test/java/UIElements/Project/SpringTest.java
@@ -0,0 +1,13 @@
+package UIElements.Project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SpringTest {
+
+    @Test
+    public void testConstructor(){
+        Actor actor = new Actor();
+        Assertions.assertTrue(true);
+    }
+}
